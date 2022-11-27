7c73091ddb5ea5a7cf405ebe18ea5316ed92ae38
Artem <lebartigor@gmail.com>
2022-11-27 15:54:51 +0300

    Vvod texta i sozdania massiva strok

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿//_____________Итоговый проект по 1 четверти____________________
-
-
+Console.WriteLine("Введите массив строк через пробел:");
+string text = Console.ReadLine();
+Console.WriteLine();
+//Console.WriteLine(text);
+string[] Words = text.Split(new char[] { ' ' });
+PrintArray(Words);
 //Создаем метод печати одномерного массива
-void PrintArray (int[] array)
+void PrintArray (string[] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++) Console.Write($"{(array[i])}, "); //печатаем
+    Console.Write("|");
+    for (int i = 0; i < array.GetLength(0); i++) Console.Write($"{(array[i])}|"); //печатаем
 }
-
